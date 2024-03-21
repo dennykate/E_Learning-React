25d0a4a14d2ace15c94eeb1ec6baf085e4e7e184
Denny_Kate <dennykate22@gmail.com>
2024-03-21 19:55:43 +0630

    feat: image changes

--- src/utils/data.js
+++ src/utils/data.js
@@ -22,48 +22,42 @@ export const classData = [
     price: "$90",
     lessons: "13 Lessons",
     students: "20 Students",
-    thumbnail:
-      "https://graphtheme.com/kintergartenreact/static/media/work1.57b5bee5.jpg",
+    thumbnail: "https://i.postimg.cc/Hx5bG9Gk/on-the.jpg",
   },
   {
     title: "Bangla Class",
     price: "$60",
     lessons: "8 Lessons",
     students: "10 Students",
-    thumbnail:
-      "https://graphtheme.com/kintergartenreact/static/media/work2.cf6a672a.jpg",
+    thumbnail: "https://i.postimg.cc/Hx5bG9Gk/on-the.jpg",
   },
   {
     title: "English Class",
     price: "$99",
     lessons: "18 Lessons",
     students: "40 Students",
-    thumbnail:
-      "https://graphtheme.com/kintergartenreact/static/media/work3.249f3a5d.jpg",
+    thumbnail: "https://i.postimg.cc/Hx5bG9Gk/on-the.jpg",
   },
   {
     title: "Math Class",
     price: "$70",
     lessons: "15 Lessons",
     students: "30 Students",
-    thumbnail:
-      "https://graphtheme.com/kintergartenreact/static/media/work4.9cdc70c5.jpg",
+    thumbnail: "https://i.postimg.cc/Hx5bG9Gk/on-the.jpg",
   },
   {
     title: "Physics Class",
     price: "$70",
     lessons: "15 Lessons",
     students: "30 Students",
-    thumbnail:
-      "https://graphtheme.com/kintergartenreact/static/media/work5.e0be6d32.jpg",
+    thumbnail: "https://i.postimg.cc/Hx5bG9Gk/on-the.jpg",
   },
   {
     title: "Chemistry Class",
     price: "$80",
     lessons: "16 Lessons",
     students: "30 Students",
-    thumbnail:
-      "https://graphtheme.com/kintergartenreact/static/media/work6.1c396ff1.jpg",
+    thumbnail: "https://i.postimg.cc/Hx5bG9Gk/on-the.jpg",
   },
 ];
 
@@ -72,77 +66,77 @@ export const teacherData = [
     name: "Devil Roy Barman",
     role: "CEO",
     image:
-      "https://graphtheme.com/kintergartenreact/static/media/teacher01.ef9900b4.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
   },
   {
     name: "Arup Barman",
     role: "CTO",
     image:
-      "https://graphtheme.com/kintergartenreact/static/media/teacher02.ecb09afe.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
   },
   {
     name: "Politkans",
     role: "CEO",
     image:
-      "https://graphtheme.com/kintergartenreact/static/media/teacher03.ea1dd076.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
   },
   {
     name: "Saiful Islam",
     role: "MANAGER",
     image:
-      "https://graphtheme.com/kintergartenreact/static/media/teacher04.fe86f51d.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
   },
   {
     name: "Munir Hayder",
     role: "SEO",
     image:
-      "https://graphtheme.com/kintergartenreact/static/media/teacher05.9d015a74.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
   },
   {
     name: "Pritom Roy",
     role: "CEO",
     image:
-      "https://graphtheme.com/kintergartenreact/static/media/teacher06.901f0774.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
   },
   {
     name: "Devil Roy Barman",
     role: "MARKETING",
     image:
-      "https://graphtheme.com/kintergartenreact/static/media/teacher07.266e5393.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
   },
   {
     name: "Nishad Boruya",
     role: "CEO",
     image:
-      "https://graphtheme.com/kintergartenreact/static/media/teacher08.a3fe896d.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
   },
 ];
 
 export const dailyUpdateData = [
   {
     thumbnail:
-      "https://graphtheme.com/kintergartenreact/static/media/blog.0419b18d.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
     title: "Helping Your Child",
     paragraph:
       "While the unit economics were a driving factor, the company says its acquisition of lidar company",
   },
   {
     thumbnail:
-      "https://graphtheme.com/kintergartenreact/static/media/blog1.3cad522c.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
     title: "Save the Children",
     paragraph:
       "company Blackmore and the integration of that tech in its self-driving stack has made the shift",
   },
   {
     thumbnail:
-      "https://graphtheme.com/kintergartenreact/static/media/blog2.8a1e528f.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
     title: "Visit the organization",
     paragraph:
       "Aurora has said its FirstLight Lidar gives it a crucial competitive advantage in high-speed self-driving",
   },
   {
     thumbnail:
-      "https://graphtheme.com/kintergartenreact/static/media/blog3.6f3bec1f.jpg",
+      "https://i.postimg.cc/vH311NbV/against-studio-background-1258-89269.jpg",
     title: "Children in International",
     paragraph:
       "At the virtual two-day conference, we’re bringing together some of the most seasoned operators",
@@ -150,12 +144,12 @@ export const dailyUpdateData = [
 ];
 
 export const galleryData = [
-  "https://graphtheme.com/kintergartenreact/static/media/service1.57b5bee5.jpg",
-  "https://graphtheme.com/kintergartenreact/static/media/footer3.c23504d6.jpg",
-  "https://graphtheme.com/kintergartenreact/static/media/footer5.dffb70d1.jpg",
-  "https://graphtheme.com/kintergartenreact/static/media/footer7.31d8c2ee.jpg",
-  "https://graphtheme.com/kintergartenreact/static/media/footer2.21177b60.jpg",
-  "https://graphtheme.com/kintergartenreact/static/media/footer4.d20e3349.jpg",
-  "https://graphtheme.com/kintergartenreact/static/media/footer6.cf28a467.jpg",
-  "https://graphtheme.com/kintergartenreact/static/media/footer8.3d3e19ef.jpg",
+  "https://i.postimg.cc/4dQcWNk8/ccessful-600nw-521875054.webp",
+  "https://i.postimg.cc/4dQcWNk8/ccessful-600nw-521875054.webp",
+  "https://i.postimg.cc/4dQcWNk8/ccessful-600nw-521875054.webp",
+  "https://i.postimg.cc/4dQcWNk8/ccessful-600nw-521875054.webp",
+  "https://i.postimg.cc/4dQcWNk8/ccessful-600nw-521875054.webp",
+  "https://i.postimg.cc/4dQcWNk8/ccessful-600nw-521875054.webp",
+  "https://i.postimg.cc/4dQcWNk8/ccessful-600nw-521875054.webp",
+  "https://i.postimg.cc/4dQcWNk8/ccessful-600nw-521875054.webp",
 ];
